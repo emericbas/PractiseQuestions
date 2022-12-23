7994df3ef975c6aa17d4c41744c1df3ae9436a03
Meric Cirem <emericbas@gmail.com>
2022-12-23 23:55:16 +0000

    IkiSayiToplamiStringde

---
+++ src/Char/CharDEgerAtamaSwitchCase.java
@@ -0,0 +1,26 @@
+package Char;
+
+public class CharDEgerAtamaSwitchCase {
+    public static void main(String[] args) {
+         /*   Soru 13-)
+        Char cevap değişkeninin değerini test edelim ve aşağıdaki eylemleri gerçekleştiren bir kod yazalım:
+        Cevap a ise, konsolda 'İsteğiniz işleniyor' mesajı yazsın
+        Yanıt b ise, konsolda 'Değerlendirmeniz için yine de teşekkür ederiz' mesajı yazsın
+        Yanıt c ise 'Üzgünüm şuanda yardımcı olamıyoruz' mesajı,
+                Yanıtın başka bir değeri için ise 'Geçersiz giriş, lütfen tekrar deneyin!' mesajı yazdırılsın.
+        Ipucu:
+        Switch konusunu hatırlayalım!
+                */
+        char cevap='+';
+        switch (cevap){
+            case 'a':
+                System.out.println("İsteğiniz işleniyor");break;
+            case 'b':
+                System.out.println("Değerlendirmeniz için yine de teşekkür ederiz");break;
+            case'c':
+                System.out.println("Üzgünüm şuanda yardımcı olamıyoruz");break;
+            default:
+                System.out.println("Geçersiz giriş, lütfen tekrar deneyin!");
+        }
+    }
+}
